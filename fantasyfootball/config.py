4127bef3ccd985490f93b8402918c7492b1b0284
Rob Mulligan <rmull61@gmail.com>
2020-08-27 09:59:05 -0400

    changed abs to rel

--- fantasyfootball/config.py
+++ fantasyfootball/config.py
@@ -4,8 +4,8 @@ import pandas as pd
 from os import path
 import numpy as np
 
-DATA_DIR = r'C:\Users\rmull\Documents\Rob\Python Projects\fantasy-football\data\raw'
-FIGURE_DIR = r'C:\Users\rmull\Documents\Rob\Python Projects\fantasy-football\figures'
+DATA_DIR = r'..\data\raw'
+FIGURE_DIR = r'..\figures'
 
 #scoring systems
 justin = {
