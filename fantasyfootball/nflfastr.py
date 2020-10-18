911aae0c89dae4d8818f6d8bf0e4a1dab9926d6b
Rob Mulligan <rmull61@gmail.com>
2020-10-18 10:12:58 -0400

    sqaped player_ids for names

--- fantasyfootball/nflfastr.py
+++ fantasyfootball/nflfastr.py
@@ -32,18 +32,17 @@ def get_nfl_fast_r_roster_data(*years):
     #df = df.loc[df['teamPlayers.status'] == 'ACT']
     return df
 
-def target_share_vs_air_yard_share(df):
+def target_share_vs_air_yard_share_transform(df):
     year = df['game_id'].str.split('_').str[0].max()
     week = df['week'].max()
-    my_cols = ['receiver', 'posteam', 'play_type', 'air_yards']
+    names = df[['receiver_id', 'receiver']].drop_duplicates().dropna()
+    my_cols = ['receiver_id', 'posteam', 'play_type', 'air_yards']
     play_type = df['play_type'] == 'pass'
     df =  df.loc[play_type, my_cols].copy()
-    df = (df.groupby(['receiver', 'posteam'])
+    df = (df.groupby(['receiver_id', 'posteam'])
             .agg(targets=('play_type', 'count'), air_yards=('air_yards', 'sum'))
             .sort_values('targets', ascending=False)
-            .reset_index()
-            .replace({'posteam' : {'LA': 'LAR'}})
-             )
+            )
     df = (df.assign(team_targets=df.groupby('posteam')['targets'].transform('sum'))
            .assign(team_air_yards=df.groupby('posteam')['air_yards'].transform('sum'))
          )
@@ -51,6 +50,10 @@ def target_share_vs_air_yard_share(df):
             .assign(air_yard_share= df['air_yards'] / df['team_air_yards'])
             .assign(year=year)
             .assign(week=week)
+            .reset_index()
+            .merge(names, how='left', on='receiver_id')
+            .drop(columns='receiver_id')
+            .set_index('receiver')
          )
     return df
 
@@ -65,7 +68,7 @@ def target_share_vs_air_yard_share_viz(df, *team_filter, n=60, x_size=20, y_size
     fig, ax = plt.subplots(figsize=(x_size, y_size))
     ax.scatter(df['target_share'], df['air_yard_share'], color=df['color'], alpha=1.0)
     text = [ax.annotate(name, xy=(x+.001, y), alpha=1.0, zorder=2) for name, x, y 
-        in zip(df['receiver'], df['target_share'], df['air_yard_share'])]
+        in zip(df.index, df['target_share'], df['air_yard_share'])]
     adjust_text(text)
     #axis formatting
     ax.set_xlabel('Target Share', fontsize=12)
@@ -95,12 +98,13 @@ def target_share_vs_air_yard_share_viz(df, *team_filter, n=60, x_size=20, y_size
 
 def carries_inside_5_yardline_transform(df):
     df = df.copy()
+    names = df[['rusher_id', 'rusher']].drop_duplicates().dropna()
     year = df['game_id'].str.split('_').str[0].max()
     week = df['week'].max()
     inside_5 = df.loc[(df['yardline_100']<5) &
              (df['play_type']=='run')]
     carries_5 = (
-    inside_5.groupby(['rusher','posteam'])[['play_id']]
+    inside_5.groupby(['rusher_id', 'posteam'])[['play_id']]
     .count()
     .reset_index()
     .sort_values(by=['play_id'],ascending=False)
@@ -109,6 +113,7 @@ def carries_inside_5_yardline_transform(df):
     carries_5 = (carries_5.assign(year=year)
                           .assign(week=week)
     )
+    carries_5 = carries_5.merge(names, on='rusher_id').drop(columns=['rusher_id']).set_index('rusher')
     return carries_5
 
 def carries_inside_5_yardline_viz(df, *team_filter, n=20, x_size=20, y_size=15, save=True):
@@ -122,12 +127,12 @@ def carries_inside_5_yardline_viz(df, *team_filter, n=20, x_size=20, y_size=15, 
     df['logo'] = df['posteam'].map(nfl_logo_espn_path_map)
     sns.set_style('whitegrid');
     fig,ax = plt.subplots(figsize=(x_size, y_size))
-    ax.barh(df['rusher'], df['play_id'], color=df['color'])
-    for carries, x, y in zip(df['play_id'], df['play_id'], df['rusher']):
+    ax.barh(df.index, df['play_id'], color=df['color'])
+    for carries, x, y in zip(df['play_id'], df['play_id'], df.index):
         ax.annotate(carries, xy=(x, y), fontsize=12)
     images = [OffsetImage(plt.imread(file_path), zoom=.1) for file_path in df['logo'].to_list()]
     x0 = [(df['play_id'].max() * 0.02)] * len(df['play_id'])
-    for im, x, y in zip(images, x0, df['rusher']):
+    for im, x, y in zip(images, x0, df.index):
         ax.add_artist(AnnotationBbox(im, xy=(x,y), frameon=False, xycoords='data'))
     #axis formatting
     ax.invert_yaxis()
