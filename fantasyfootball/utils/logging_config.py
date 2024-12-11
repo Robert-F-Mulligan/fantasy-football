a4362416587c3158b8796b04d97b1f3032048d8f
Rob Mulligan <robert.f.mulligan@gmail.com>
2024-12-11 10:11:54 -0500

    added utf-8 encoding

--- fantasyfootball/utils/logging_config.py
+++ fantasyfootball/utils/logging_config.py
@@ -1,17 +1,24 @@
 import json
 import logging.config
+import sys
 
 def setup_logging(path=r'config\logger.json', default_level=logging.INFO):
     """Setup logging configuration from a JSON file."""
     try:
-        with open(path, 'rt') as f:
+        with open(path, 'rt', encoding='utf-8') as f:
             config = json.load(f)
         logging.config.dictConfig(config)
     except Exception as e:
         print(f"Error in loading logging configuration: {e}")
         logging.basicConfig(level=default_level)
 
-    # logging carveouts
+    try:
+        sys.stdout.reconfigure(encoding='utf-8')
+    except AttributeError:
+        # Fallback for older Python versions without `reconfigure`
+        pass
+
+    # Logging carveouts
     logging.getLogger('snowflake.connector').setLevel(logging.WARNING)
 
 if __name__ == '__main__':
@@ -20,5 +27,5 @@ if __name__ == '__main__':
     # Now you can use logging in your application
     logger = logging.getLogger(__name__)
 
-    logger.info("This is an info message")
+    logger.info("This is an info message with Unicode: 🌟")
     logger.debug("This is a debug message")
