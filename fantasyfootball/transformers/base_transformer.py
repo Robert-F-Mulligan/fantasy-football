53f930be22b0402373cf631bfe0615edef0e88a0
Rob Mulligan <robert.f.mulligan@gmail.com>
2024-12-13 11:58:26 -0500

    Moved helper methods to base

--- fantasyfootball/transformers/base_transformer.py
+++ fantasyfootball/transformers/base_transformer.py
@@ -23,3 +23,25 @@ class BaseTransformer(ABC):
         :return: Transformed DataFrame.
         """
         pass
+
+    def _rename_columns(self, rename_map: dict):
+        """Renames columns based on a provided mapping."""
+        logger.debug("Renaming columns using provided map.")
+        self.dataframe = self.dataframe.rename(columns=rename_map)
+        return self
+
+    def _drop_columns(self, columns: list = None):
+        """Drops columns"""
+        logger.debug("Cleaning columns: dropping unwanted columns.")
+        if columns:
+            self.dataframe = self.dataframe.drop(columns=columns)
+        return self
+
+    def _reindex_and_fill(self, column_order: list, fill_value=0, dtype_map: dict = None):
+        """Reorders columns, fills missing values, and casts data types."""
+        logger.debug("Reindexing columns and filling missing values.")
+        self.dataframe = self.dataframe.reindex(columns=column_order, fill_value=fill_value).fillna(fill_value)
+        if dtype_map:
+            self.dataframe = self.dataframe.astype(dtype_map)
+        return self
+    
