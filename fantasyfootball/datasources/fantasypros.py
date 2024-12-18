ad754e81671386d8073f81fb6bb964d89c92365e
Rob Mulligan <robert.f.mulligan@gmail.com>
2024-12-18 14:01:54 -0500

    fixed self.parser in the extract_datetime method

--- fantasyfootball/datasources/fantasypros.py
+++ fantasyfootball/datasources/fantasypros.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 import pandas as pd
 from fantasyfootball.connectors.selenium_connector import SeleniumConnector
 from fantasyfootball.parsers.html_parser import HTMLParser
@@ -21,11 +22,60 @@ class FantasyProsDatasource(BaseDataSource):
             self._get_data_from_html(endpoint=endpoint,
                                        table_id=table_id)
                 .pipe(self._clean_columns)
-        ) 
+                .assign(as_of_date= self._extract_datetime())
+        )
+
+    def _extract_datetime(self) -> str:
+        """
+        Extracts the player's position from the soup object.
+        
+        :return: Returns a datetime object.
+        """
+        
+        try:
+            if not self.parser.soup:
+                raise RuntimeError("Parser content must be parsed before extracting datetime.")
+        
+            time_tags = self.parser.extract("time")
+
+            date_text = next(
+            (tag.get_text(strip=True) for tag in time_tags if tag.has_attr("datetime")),
+            None
+            )
+            if date_text:
+                date_object = datetime.strptime(date_text, '%b %d, %Y')
+                return date_object.isoformat()
+            else:
+                logger.warning("No valid datetime text found in <time> tags.")
+                return None
+        except ValueError as ve:
+            logger.error(f"Invalid date format for datetime extraction: {ve}")
+            return None
+        except Exception as e:
+            logger.error(f"Unexpected error during datetime extraction: {e}")
+            return None
 
 if __name__ == "__main__":
     from fantasyfootball.utils.logging_config import setup_logging
     setup_logging()
+
+    pos = 'te'
+
+    fp_map =  {
+        'projections':
+        {'table_id': 'data',
+         'endpoint': f'nfl/projections/{pos}.php?week=draft&scoring=PPR&week=12',
+        },
+         'weekly_rank':
+         {'table_id': 'ranking-table',
+         'endpoint': f'nfl/rankings/ppr-{pos}.php',
+        },
+        'draft':
+        {'table_id': 'ranking-table',
+         'endpoint': f'nfl/rankings/ppr-cheatsheets.php'},
+    }
+    key = 'draft'
+    config = fp_map.get(key)
    
     BASE_URL =  'https://www.fantasypros.com'
     DRIVER_PATH = r'C:\Users\rmull\python-projects\fantasy-football\chrome-driver\chromedriver.exe'
@@ -34,6 +84,8 @@ if __name__ == "__main__":
     #'table', id='data'
     with connector:
         data = FantasyProsDatasource(connector, parser=parser)
-        href = 'nfl/projections/qb.php?week=draft&scoring=PPR&week=12'
-        df = data.get_data(endpoint=href, table_id='data')
+        href = config.get('endpoint')
+        table_id = config.get('table_id')
+        df = data.get_data(endpoint=href, table_id=table_id)
+        df.to_csv(f'{key}_datasource.csv', index=False)
         print(df.head())
