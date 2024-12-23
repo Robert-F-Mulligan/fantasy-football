4aec7b5e95ca14100a8c2e92d8bb224cf96952e8
Rob Mulligan <robert.f.mulligan@gmail.com>
2024-12-23 16:37:54 -0500

    refactored

--- fantasyfootball/strategies/pfr_gbg_strategy.py
+++ fantasyfootball/strategies/pfr_gbg_strategy.py
@@ -1,12 +1,7 @@
 import logging
 import time
-from typing import Iterable
 import pandas as pd
 from fantasyfootball.strategies.base_strategy import BaseStrategy
-from fantasyfootball.connectors.requests_connector import RequestsConnector
-from fantasyfootball.transformers.profootballreference_transformer import GameByGameTransformer
-from fantasyfootball.parsers.html_parser import HTMLParser
-from fantasyfootball.datasources.profootballreference import ProFootballReferenceDataSource
 from fantasyfootball.factories.strategy_factory import StrategyFactory
 from fantasyfootball.utils.logging_config import setup_logging
 
@@ -14,21 +9,18 @@ setup_logging()
 
 logger = logging.getLogger(__name__)
 
-BASE_URL = "https://www.pro-football-reference.com"
-
 @StrategyFactory.register('game_by_game')
 class ProFootballReferenceGbGStrategy(BaseStrategy):
     def __init__(self, combined_config: dict, **kwargs):
         super().__init__(combined_config, **kwargs)
         """
-        Initializes the controller with the data source and transformer.
-        
-        :param datasource: Data source instance for Pro Football Reference.
-        :param transformer: Transformer instance for data processing.
+        Initialize the strategy with its combined configuration.
+        :param combined_config: The combined configuration for the datasource and dataset.
+        :param kwargs: Additional parameters for the strategy (optional).
         """
+        self.all_data = []
 
-
-    def run(self, years: Iterable[int], max_players_per_year: int = None, sleep: int = 5) -> pd.DataFrame:
+    def run(self, sleep: int = 5) -> pd.DataFrame:
         """
         Processes data for multiple years.
         
@@ -37,21 +29,28 @@ class ProFootballReferenceGbGStrategy(BaseStrategy):
         :param sleep: Time to wait (in seconds) between years to avoid rate-limiting.
         :return: A concatenated DataFrame containing all the processed data.
         """
-        all_data = []
+        years = range(self.min_year, self.max_year + 1)
 
-        for year in years:
-            try:
-                logger.info(f"Processing year: {year}")
-                year_df = self._run_one_year(year, max_players_per_year, sleep=sleep)
-                if not year_df.empty:
-                    all_data.append(year_df)
-                time.sleep(sleep)  # Sleep between each year's processing
-            except Exception as e:
-                logger.error(f"Failed to process year {year}: {e}")
+        logger.info(f"Starting data processing for years {years} for {self.dataset_name} dataset.")
 
-        return pd.concat(all_data, ignore_index=True) if all_data else pd.DataFrame()
+        with self.connector as connector:
 
-    def _run_one_year(self, year: int, max_players: int = None, sleep: int = 5) -> pd.DataFrame:
+            for year in years:
+                try:
+                    logger.info(f"Processing year: {year}")
+                    year_df = self._run_one_year(year,  
+                                                 sleep=sleep, 
+                                                 connector=connector,
+                                                 parser=self.parser)
+                    if not year_df.empty:
+                        self.all_data.append(year_df)
+                    time.sleep(sleep)  # Sleep between each year's processing
+                except Exception as e:
+                    logger.error(f"Failed to process year {year}: {e}")
+
+            return pd.concat(self.all_data, ignore_index=True) if self.all_data else pd.DataFrame()
+
+    def _run_one_year(self, year: int, sleep: int = 5, **kwargs) -> pd.DataFrame:
         """
         Processes data for a single year.
         
@@ -59,16 +58,21 @@ class ProFootballReferenceGbGStrategy(BaseStrategy):
         :param max_players: Maximum number of players to process.
         :return: A DataFrame containing the processed data for the year.
         """
-        endpoint = self.get_endpoint_for_year(year)
-        player_hrefs = self.datasource.get_player_hrefs(endpoint, table_id='fantasy')
+        endpoint = self.year_endpoint_template.format(year=year)
+        player_hrefs = self.datasource.get_player_hrefs(endpoint, 
+                                                        table_id=self.href_table_id,
+                                                        **kwargs)
 
-        if max_players:
-            player_hrefs = player_hrefs[:max_players]
+        if self.max_players_per_year:
+            player_hrefs = player_hrefs[:self.max_players_per_year]
+
         year_data = []
         for player_href in player_hrefs:
             try:
                 last_name_letter, player_id = self.datasource._player_id_transform(player_href)
-                player_endpoint = self.get_endpoint_for_year_and_player(year, last_name_letter, player_id)
+                player_endpoint = self.endpoint_template.format(year=year, 
+                                                                last_name_letter=last_name_letter,
+                                                                player_id=player_id)
 
                 additional_cols = {
                     'player_id': player_id,
@@ -78,9 +82,13 @@ class ProFootballReferenceGbGStrategy(BaseStrategy):
                 }
 
                 player_table = (self.datasource
-                                .get_data(endpoint=player_endpoint, table_id="stats")
+                                .get_data(endpoint=player_endpoint, 
+                                          table_id=self.table_id,
+                                          **kwargs)
                                 .pipe(self.datasource.assign_columns, **additional_cols)
                 )
+                logger.debug(f"Constructed player endpoint: {player_endpoint}")
+                logger.debug(f"Columns added: {additional_cols}")
 
                 transformed_data = self.transformer.transform(dataframe=player_table)
                 year_data.append(transformed_data)
@@ -91,44 +99,6 @@ class ProFootballReferenceGbGStrategy(BaseStrategy):
         
         return pd.concat(year_data, ignore_index=True) if year_data else pd.DataFrame()
 
-    def get_endpoint_for_year(self, year: int) -> str:
-        """Generate the endpoint URL for a specific year."""
-        return f"years/{year}/fantasy.htm"
-    
-    def get_endpoint_for_year_and_player(self, year: int, last_name_letter: str, player_id: str) -> str:
-        return f'/players/{last_name_letter}/{player_id}/gamelog/{year}/'
-    
-def main(start_year: int, end_year: int, max_players: int = None, sleep: int = 5, output_file: str = "output.csv"):
-    """
-    Main function to process data for a range of years and save the result to a CSV file.
-
-    :param start_year: Starting year of the range.
-    :param end_year: Ending year of the range.
-    :param max_players: Maximum number of players to process per year.
-    :param sleep: Time to wait (in seconds) between years to avoid rate-limiting.
-    :param output_file: Name of the file to save the output.
-    """
-    years = range(start_year, end_year + 1)
-
-    connector = RequestsConnector(base_url=BASE_URL)
-
-    with connector as conn:
-        parser = HTMLParser()
-        datasource = ProFootballReferenceDataSource(connector=conn, parser=parser)
-        transformer = GameByGameTransformer()
-        controller = ProFootballReferenceGbGStrategy(datasource=datasource, transformer=transformer)
-
-        try:
-            logger.info(f"Starting data processing for years {start_year} to {end_year}...")
-            final_df = controller.run(years=years, max_players_per_year=max_players, sleep=sleep)
-
-            if not final_df.empty:
-                final_df.to_csv(output_file, index=False)
-                logger.info(f"Data successfully saved to {output_file}.")
-            else:
-                logger.warning("No data to save. The resulting DataFrame is empty.")
-        except Exception as e:
-            logger.error(f"An error occurred during processing: {e}")
 
 if __name__ == "__main__":
-    main(start_year=2022, end_year=2022, max_players=4, sleep=10)
+    pass
