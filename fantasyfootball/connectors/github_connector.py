bcc765faae5deaff03fbd21fce64f6e2d93d90e2
Rob Mulligan <robert.f.mulligan@gmail.com>
2024-12-24 12:40:12 -0500

    removed timeout

--- fantasyfootball/connectors/github_connector.py
+++ fantasyfootball/connectors/github_connector.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 @ConnectorFactory.register("github")
 class GitHubConnector(BaseConnector):
-    def __init__(self, base_url: str = None, timeout: int = 10):
+    def __init__(self, base_url: str = None):
         """
         Initialize the connector with base URL and timeout settings.
 
@@ -17,27 +17,21 @@ class GitHubConnector(BaseConnector):
         :param timeout: Timeout for the request (default 10 seconds).
         """
         self.base_url = base_url
-        self.timeout = timeout
 
     @retry_decorator(retries=3, delay=2, backoff_factor=2)
-    def fetch(self, endpoint: str, connector=None, chunksize: int = None) -> pd.DataFrame:
+    def fetch(self, endpoint: str, chunksize: int = None) -> pd.DataFrame:
         """
         Fetch CSV data from the specified GitHub URL endpoint.
 
         :param endpoint: The GitHub repository path to the CSV file.
+        :param chunksize: Number of rows to read per chunk. If None, reads the entire file.
         :return: A pandas DataFrame containing the CSV data.
         """
-        self.connector = connector or self.connector
-
-        if not self.connector:
-            raise ValueError("Connector must be provided either at init or in get_data.")
-        
         url = f"{self.base_url}/{endpoint.lstrip('/')}"
         try:
             logger.info(f"Fetching CSV data from URL: {url}")
             data = pd.read_csv(
                 url,
-                timeout=self.timeout,
                 compression='gzip',
                 low_memory=False,
                 chunksize=chunksize
